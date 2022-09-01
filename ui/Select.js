eb3f23b354e6036b47764b6c934d6000b61cf60e
alex <itreum@gmail.com>
2022-09-01 19:36:32 +0300

    v

--- ui/Select.js
+++ ui/Select.js
@@ -2,7 +2,7 @@ export default{
     name:"Select",
     data(){
         return {
-
+            showSort:false
         }
     },
     props:{
@@ -13,14 +13,26 @@ export default{
         changeSort(event){
             console.log(event.target.value)
             this.$emit('update:modelValue',event.target.value)
+        },
+        openSort(){
+            this.showSort=!this.showSort
         }
     },
     computed:{
      
     },
     template:/*html*/`
-        <select name="" id="" @change="changeSort" class="select">
+        <!--<select name="" id="" @change="changeSort" class="select">
             <option v-for="item in selects" :key="item.value" :value="item.value">{{item.name.toUpperCase()}}</option>         
-        </select>
+        </select>-->
+
+        <div class="" @click="openSort">{{modelValue}}</div>
+        
+        <div class="sort" v-show="showSort">
+            <div class="" v-for="item in selects" >
+                <input readonly type="text" :key="item.value" :value="item.value" @click="changeSort" @click="openSort">
+            </div>
+        </div>
+        <div class="modal" v-show="showSort"></div>
     `   
 }
