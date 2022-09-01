878c121caa330fabd79eaae4b1317f1c3e3e4a1a
alex <itreum@gmail.com>
2022-09-01 14:34:15 +0300

    v4

--- components/Slider.js
+++ components/Slider.js
@@ -1,12 +1,39 @@
 export default{
     name:'Slider',
+    data(){
+        return{
+            banners:[
+                {link:"../public/img/slide_1.png",name:"slide_1"},
+                {link:"../public/img/slide_2.jpeg",name:"slide_2"},
+                {link:"../public/img/slide_3.jpeg",name:"slide_3"},
+                {link:"../public/img/slide_4.jpeg",name:"slide_4"},
+                {link:"../public/img/slide_5.jpg",name:"slide_5"},
+            ]
+        }
+    },
     methods:{
 
     },
     template: /*html*/ `
-    <div class="slider-banner">
-        <img src="../public/img/slide_1.png" alt="">
+        <!--<div class="slider-banner">
+            
+        </div>-->
+    <div class="slider" data-slider="itc-slider" data-sipe="false">
+        <div class="slider__wrapper">
+            <div class="slider__items">
+                <div class="slider__item" v-for="item in banners">
+                    <img :src="item.link" :alt="item.name" :key="item.name">
+                </div>
+                
+            </div>
+        </div>
+        <!-- Кнопки для перехода к предыдущему и следующему слайду -->
+        <button class="slider__control" data-slide="prev"></button>
+        <button class="slider__control" data-slide="next"></button>
+        <ol class="slider__indicators">
+            <li :data-slide-to="index" v-for="(item,index) in banners"></li>
+            
+        </ol>
     </div>
-    
     `
 }
